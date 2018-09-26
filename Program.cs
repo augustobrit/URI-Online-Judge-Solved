6f9418b82a96bf0e0ed3e728a4c37e42c85ebf07
Augusto Brito <augustobrito@outlook.com>
2018-09-26 11:52:57 -0300

    Fix dotnet

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using Beginner;
+
+namespace uri_online_judge_solved
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            AgeInDays.Init();
+        }
+    }
+}
